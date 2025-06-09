5d5af9ea922d9fd4628cae8181109b2dff228c41
K10-K10 <keito.tottori@gmail.com>
2025-06-10 00:20:55 +0900

    Add list-test
    Show all notes in note.txt

--- note-rust/src/main.rs
+++ note-rust/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(dead_code)]
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
+use std::path::Path;
+use std::{fs, mem};
+
+struct Item {
+    num: u32,
+    title: String,
+    body: String,
+    check: bool,
+}
+
+fn list(path: File) -> Result<(), Box<dyn std::error::Error>> {
+    let reader = BufReader::new(path);
+
+    for line_result in reader.lines() {
+        let line = line_result?;
+        println!("{}", line)
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("../note.txt")?;
+    list(file)
 }
