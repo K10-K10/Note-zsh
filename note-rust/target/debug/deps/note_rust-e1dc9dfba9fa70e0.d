3216a907678c76fc4894164ab6f1103e1ed07f80
K10-K10 <keito.tottori@gmail.com>
2025-06-10 00:24:45 +0900

    remove cargofile

--- note-rust/target/debug/deps/note_rust-e1dc9dfba9fa70e0.d
+++
@@ -1,5 +0,0 @@
-/home/k10-k10/projects/zsh/Note-zsh/note-rust/target/debug/deps/note_rust-e1dc9dfba9fa70e0: src/main.rs
-
-/home/k10-k10/projects/zsh/Note-zsh/note-rust/target/debug/deps/note_rust-e1dc9dfba9fa70e0.d: src/main.rs
-
-src/main.rs:
