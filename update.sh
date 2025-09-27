a269df2998dccda92ee2642d993c6cb7582d6dbc
K10-K10 <keito.tottori@gmail.com>
2025-09-28 00:43:12 +0900

    Eidt:Fix update shell's bug

--- update.sh
+++ update.sh
@@ -9,7 +9,11 @@ mkdir -p "$INSTALL_DIR" "$BIN_DIR"
 echo "Updating Note-zsh..."
 curl -L "$ARCHIVE_URL" | tar -xz -C "$INSTALL_DIR"
 
+mv "$BIN_DIR/note.txt" "$HOME"
+
 cp "$INSTALL_DIR/note-rust/note-rust" "$BIN_DIR/note"
 chmod +x "$BIN_DIR/note"
+
+mv "$HOME/note.txt" "BIN_DIR/note.txt"
 
 echo "Note-zsh updated! Run with 'note tui'"
